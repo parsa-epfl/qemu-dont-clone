88187a1d1ebff6a4ec35aaef7423930fb0b41f6e
Rafael Ulises Luzius Pizarro Solar <rafael.pizarrosolar@epfl.ch>
2019-09-22 19:06:18 +0200

    Magic instruction, generate for instructions HINT 90-127

--- target/arm/translate-a64.c
+++ target/arm/translate-a64.c
@@ -1446,6 +1446,14 @@ static void handle_hint(DisasContext *s, uint32_t insn,
     case 5: /* SEVL */
         /* we treat all as NOP at least for now */
         return;
+#if defined(CONFIG_FLEXUS)
+    case 90:  case 91:  case 92:  case 93:  case 94:  case 95:  case 96:  case 97:  case 98:  case 99:
+    case 100: case 101: case 102: case 103: case 104: case 105: case 106: case 107: case 108: case 109:
+    case 110: case 111: case 112: case 113: case 114: case 115: case 116: case 117: case 118: case 119:
+    case 120: case 121: case 122: case 123: case 124: case 125: case 126: case 127:
+        GEN_HELPER(qflex_magic_insn)(tcg_const_i32(selector));
+        return;
+#endif /* CONFIG_FLEXUS */
     default:
         /* default specified as NOP equivalent */
         return;
